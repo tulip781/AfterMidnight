e4b8aa3e601d2075b679113373feab8a6861d1ec
Oliver Inglis <ollie@theinglistwins.com>
2020-06-23 15:02:43 +0100

    working columns

--- src/js/app.js
+++ src/js/app.js
@@ -39,3 +39,50 @@ function stopColor() {
   land.classList.add('fade-in');
   window.setTimeout( yo, 2000);
 }
+
+const images = document.querySelectorAll('img');
+
+
+
+const moveIn = (e) => {
+  e.classList.add('active');
+}
+
+const moveOut = (e) => {
+  e.classList.remove('active');
+}
+
+const isScrolledIntoView = (el) => {
+    let rect = el.getBoundingClientRect();
+    console.log(rect);
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+    let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    let visible = (window.innerHeight - elemTop) >= (rect.height / 8)
+    return visible;
+}
+
+const debounce = (func, wait = 50) => {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
+
+const checkSlide = (e) => {
+  images.forEach((image) => {
+    let test = image.getBoundingClientRect()
+    let top = test.top;
+    let bottom = test.bottom;
+    if (isScrolledIntoView(image)) {
+      return moveIn(image);
+    } else if (top >= window.innerHeight || bottom <= 0) {
+      moveOut(image)
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
